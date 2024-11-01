fa6d074a8c099051a1e7ebb606319c7c104a85ad
gabriel <tagliamentogameplays@gmail.com>
2024-11-01 05:03:17 -0300

    Creating asset folder inserting all assets Creating the menu : background, sound and fonts Creating the const file

--- code/Game.py
+++ code/Game.py
@@ -1,22 +1,15 @@
 import pygame
 from code.Menu import Menu
+from code.Const import WIN_WIDTH, WINDOW_HEIGHT
+
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WINDOW_HEIGHT))
 
-    def run(self ):
+    def run(self):
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-
-
-
-            # Check for all events
-            #for event in pygame.event.get():
-            #if event.type == pygame.QUIT:
-            # pygame.quit()  # Close window
-            #quit()  # end pygame
